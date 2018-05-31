16ab364d9110d4f27ec66541553477c7fa1d651c
madhavparekh <madhavparekh@gmail.com>
2018-05-31 00:56:31 -0700

    started work on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+import Scrape from './components/Scrape';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			scrapeBtnClicked: false,
+		};
+		this.handler = this.handler.bind(this);
+	}
+	handler() {
+		this.setState({
+			scrapeBtnClicked: !this.state.scrapeBtnClicked,
+		});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Jumbotron className="m-2 p-2">
+					<h1 className="app-name">Twitter Moments Scrapper</h1>
+					<p className="lead">
+						This app scrapes News articles from Twitter Moments. Try it out
+					</p>
+					<hr className="my-2" />
+					<p className="lead">
+						<Scrape btnClicked={this.handler} />
+					</p>
+				</Jumbotron>
+				{this.state.scrapeBtnClicked && <Button>Render Tweets</Button>}
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/components/Scrape.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import Aux from '../hoc/Aux';
+
+class Scrape extends Component {
+	handleBtnClick = async (e) => {
+		e.preventDefault();
+		var res = await fetch('/scrape', {
+			method: 'GET',
+		});
+		if (res.status === 401) {
+			this.setState({ fail: true });
+		} else if (res.status === 200) {
+			this.props.btnClicked();
+		}
+	};
+
+	render() {
+		return (
+			<Aux>
+				<p className="lead">
+					<Button color="primary" onClick={this.handleBtnClick}>
+						Scrape
+					</Button>
+				</p>
+			</Aux>
+		);
+	}
+}
+
+export default Scrape;
