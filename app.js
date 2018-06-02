9cad360bba51412bd06bb7816be0326804b81b5c
madhavparekh <madhavparekh@gmail.com>
2018-06-02 10:00:10 -0700

    heroku fix

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to the Mongo DB
 var databaseUri = 'mongodb://localhost/TwitterMomentScrapper';
