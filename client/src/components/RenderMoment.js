e3ec60bf98b5556c3863b4d71ad38af6c14d68a5
madhavparekh <madhavparekh@gmail.com>
2018-06-02 07:49:20 -0700

    Rendering each moment with btn to save

---
+++ client/src/components/RenderMoment.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import {
+	Button,
+	ButtonGroup,
+	Alert,
+	Card,
+	CardBody,
+	Collapse,
+	CardImg,
+	CardText,
+	CardLink,
+} from 'reactstrap';
+import Aux from '../hoc/Auxilary';
+
+class RenderMoment extends Component {
+	constructor(props) {
+		super(props);
+		this.toggle = this.toggle.bind(this);
+		this.state = {
+			collapse: false,
+			isSaved: false,
+		};
+	}
+
+	onBtnClick = async (e) => {
+		//e.preventDefault();
+
+		this.props.item.isSaved = true;
+		console.log(this.props.item);
+
+		var res = await fetch(`/moments/save/${this.props.item._id}`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			credentials: 'same-origin',
+			body: JSON.stringify(this.props.item),
+		});
+		if (res.status === 401) {
+			this.setState({ fail: true });
+		} else if (res.status === 200) {
+			res = await res.json();
+			console.log(res);
+			this.setState({
+				isSaved: !this.state.isSaved,
+			});
+			//this.props.btnClicked();
+		}
+	};
+
+	toggle() {
+		this.setState({ collapse: !this.state.collapse });
+	}
+
+	render() {
+		return (
+			<Aux>
+				<div className="m-2">
+					<Alert
+						color="primary"
+						onClick={this.toggle}
+						style={{ marginBottom: '1rem' }}
+					>
+						<h5>{this.props.item.title}</h5>
+					</Alert>
+					<Collapse isOpen={this.state.collapse}>
+						<Card>
+							<CardBody>
+								<CardText>{this.props.item.description}</CardText>
+								<ButtonGroup>
+									<Button
+										color="secondary"
+										className="mx-1"
+										href={this.props.item.link}
+										target="_blank"
+									>
+										Article Link
+									</Button>
+									<Button
+										color="secondary"
+										className="mx-1"
+										href={this.props.item.img}
+										target="_blank"
+									>
+										Image Link
+									</Button>
+									<Button color="secondary" className="mx-1">
+										Likes: {this.props.item.likes}
+									</Button>
+								</ButtonGroup>
+							</CardBody>
+							<div className="text-center">
+								<Button
+									className="col-sm-1 m-2"
+									color="info"
+									onClick={() => this.onBtnClick(1)}
+									active={this.state.rSelected === 1} 
+									disabled={this.state.isSaved}
+								>
+									Save
+								</Button>
+							</div>
+						</Card>
+					</Collapse>
+				</div>
+			</Aux>
+		);
+	}
+}
+
+export default RenderMoment;
