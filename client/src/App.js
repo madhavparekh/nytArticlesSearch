b48bc29e281e6890872dd7e6f464bd17be9a657f
madhavparekh <madhavparekh@gmail.com>
2018-06-02 22:23:52 -0700

    saved or all article displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
 import { Jumbotron, Button } from 'reactstrap';
+
 import Scrape from './components/Scrape';
 import SacBeeLatest from './containers/SacBeeLatest';
-import logo from './logo.svg';
-import './App.css';
+import SavedArticlesBtn from './components/SavedArticlesBtn';
+import SavedArticles from './containers/SavedArticles';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			scrapeBtnClicked: false,
+			scrapeBtnClicked: 0,
+			displaySaved: false,
 		};
 		this.handler = this.handler.bind(this);
+		this.savedHandler = this.savedHandler.bind(this);
 	}
 	handler() {
 		this.setState({
-			scrapeBtnClicked: !this.state.scrapeBtnClicked,
+			scrapeBtnClicked: this.state.scrapeBtnClicked + 1,
+		});
+	}
+
+	savedHandler(action) {
+		this.setState({
+			displaySaved: action,
 		});
 	}
 
 	render() {
 		return (
-			<div className="App">
+			<div className="App text-center">
 				<Jumbotron className="m-2 p-2">
 					<h1 className="app-name">Sac Bee Latest News Scrapper</h1>
 					<p className="lead">
@@ -29,11 +38,19 @@ class App extends Component {
 					</p>
 					<hr className="my-2" />
 					<p className="lead">
-						<Scrape btnClicked={this.handler} />
+						<Scrape
+							btnClicked={this.handler}
+							savedHandler={this.savedHandler}
+						/>
+						<SavedArticlesBtn savedHandler={this.savedHandler} />
 					</p>
 				</Jumbotron>
 				<div className="col-sm-9 m-auto">
-					{this.state.scrapeBtnClicked && <SacBeeLatest />}
+					{!this.state.displaySaved ? (
+						<SacBeeLatest update={this.state.scrapeBtnClicked} />
+					) : (
+						<SavedArticles />
+					)}
 				</div>
 			</div>
 		);
