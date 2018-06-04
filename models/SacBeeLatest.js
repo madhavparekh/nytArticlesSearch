c5b773563cb05c4ad373e731527a4a42b6c2e281
madhavparekh <madhavparekh@gmail.com>
2018-06-04 10:35:11 -0700

    retyped comment to comments array

--- models/SacBeeLatest.js
+++ models/SacBeeLatest.js
@@ -40,10 +40,10 @@ var SacBeeLatestSchema = new Schema({
 	// `comment` is an object that stores a Comment id
 	// The ref property links the ObjectId to the Comment model
 	// This allows us to populate the SacBeeLatest with an associated Comment
-	comment: {
+	comments: [{
 		type: Schema.Types.ObjectId,
 		ref: 'Comment',
-	},
+	}],
 });
 
 // This creates our model from the above schema, using mongoose's model method
