11f0b36471cfba1850ae13da919fd58ba85c8328
madhavparekh <madhavparekh@gmail.com>
2018-06-04 19:04:18 -0700

    CRUD appropriate changes, clean up, route handles comment delete

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@ var db = require('../models');
 
 // A GET route for scraping the echoJS website
 router.get('/scrape', function(req, res) {
-	//console.log('scraping');
 	// First, we grab the body of the html with request
 	axios.get('http://www.sacbee.com/latest-news/').then(function(response) {
 		// Then, we load that into cheerio and save it to $ for a shorthand selector
@@ -24,8 +23,6 @@ router.get('/scrape', function(req, res) {
 		$('.teaser').each(function(i, element) {
 			// Save an empty result object
 			var result = {};
-
-			//console.log('############line 29:##########');
 
 			// Add the text and href of every link, and save them as properties of the result object
 			result.title = $(this)
@@ -45,12 +42,7 @@ router.get('/scrape', function(req, res) {
 				.children('div')
 				.children('a')
 				.children('img')
-				.attr('src');
-
-			//result.isSaved = false;
-
-			// Create a new Article using the `result` object built from scraping
-			//	console.log('#####Line 62 \n', result, '\n#####');
+				.attr('data-proxy-image');
 
 			db.SacBeeLatest.findOneAndUpdate({ link: result.link }, result, {
 				upsert: true,
@@ -58,7 +50,7 @@ router.get('/scrape', function(req, res) {
 			})
 				.then(function(dbSacBeeLatest) {
 					// View the added result in the console
-					console.log(dbSacBeeLatest);
+					//console.log(dbSacBeeLatest);
 				})
 				.catch(function(err) {
 					// If an error occurred, send it to the client
@@ -104,13 +96,11 @@ router.get('/latest/saved', function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Comment
-router.post('/latest/save/:id', function(req, res) {
-	console.log('line100', req.body);
+router.put('/latest/save/:id', function(req, res) {
 	// Create a new Comment and pass the req.body to the entry
 	db.SacBeeLatest.findByIdAndUpdate(req.params.id, req.body, { new: true })
 		.then(function(dbSacBeeLatest) {
 			// If we were able to successfully update an Article, send it back to the client
-			console.log(dbSacBeeLatest);
 			res.json(dbSacBeeLatest);
 		})
 		.catch(function(err) {
@@ -124,10 +114,9 @@ router.get('/latest/saved/comments/:id', function(req, res) {
 	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
 	db.SacBeeLatest.findOne({ _id: req.params.id })
 		// ..and populate all of the Comments associated with it
-		.populate({path: 'comments', options: {sort: {_id: -1}}})
+		.populate({ path: 'comments', options: { sort: { _id: -1 } } })
 		.then(function(dbSacBeeLatest) {
 			// If we were able to successfully find an Article with the given id, send it back to the client
-			console.log(dbSacBeeLatest);
 			res.json(dbSacBeeLatest);
 		})
 		.catch(function(err) {
@@ -156,6 +145,26 @@ router.post('/latest/saved/comment/:id', function(req, res) {
 		})
 		.catch(function(err) {
 			// If an error occurred, send it to the client
+			res.json(err);
+		});
+});
+
+router.delete('/saved/:id/comment/:comm_id', (req, res) => {
+	db.Comment.findByIdAndRemove(req.params.comm_id)
+		.then((dbComment) => {
+			return db.SacBeeLatest.findByIdAndUpdate(
+				{ _id: req.params.id },
+				{ $pull: { comments: dbComment._id } },
+				{ new: true }
+			);
+		})
+		.then((dbSacBeeLatest) => {
+			const response = {
+				message: 'Comment successfully deleted',
+			};
+			return res.status(200).send(response);
+		})
+		.catch((err) => {
 			res.json(err);
 		});
 });
