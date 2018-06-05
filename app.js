af40e9a3523792ae0b85058b2403beeaaea19ab1
madhavparekh <madhavparekh@gmail.com>
2018-06-04 19:02:00 -0700

    summer clean up

--- app.js
+++ app.js
@@ -32,10 +32,6 @@ db.once('open', () => {
 
 var app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
