3836ab2cead41a3a3ed67682e965b0bf03e96e80
madhavparekh <madhavparekh@gmail.com>
2018-06-18 19:02:05 -0700

    socket.io and modal on saved

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
-import Home from "../src/containers/Home";
-import SavedArticles from '../src/containers/SavedArticles'
+import { Modal, ModalHeader } from 'reactstrap';
+
+import io from 'socket.io-client';
+
+import Home from '../src/containers/Home';
+import SavedArticles from '../src/containers/SavedArticles';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			saved: true,
+			endpoint: 'http://127.0.0.1:3001',
+			saved: false,
+			modal: false,
 		};
 	}
 
-	render() {
-		return <Router>
-		<Switch>
-			<Route exact path='/' component={Home}/>
-			<Route exact path='/SavedArticles' component={SavedArticles}/>
-		</Switch>
+	componentDidMount() {
+		const { endpoint } = this.state;
+		const socket = io(endpoint);
+		socket.on('connection', (savedFlag) => {
+			this.setState({ modal: savedFlag });
+		});
+	}
 
-		</Router>;
+	toggle = () => {
+		this.setState({ modal: !this.state.modal });
+	};
+
+	render() {
+		return (
+			<Router>
+				<div className="container">
+					<Navbar color="secondary" dark expand="md">
+						<NavbarBrand href="/">NY Times - Search Articles</NavbarBrand>
+						<Nav className="ml-auto" navbar>
+							<NavItem>
+								<NavLink href="/">Search</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink href="/SavedArticles/">Saved Articles</NavLink>
+							</NavItem>
+						</Nav>
+					</Navbar>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/SavedArticles" component={SavedArticles} />
+
+					<Modal isOpen={this.state.modal} toggle={this.toggle}>
+						<ModalHeader toggle={this.toggle}>
+							Some one saved an article!
+						</ModalHeader>
+					</Modal>
+				</div>
+			</Router>
+		);
 	}
 }
 
