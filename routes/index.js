69432b5ae81e3865a89fe8aedcf5580757ae1783
madhavparekh <madhavparekh@gmail.com>
2018-06-18 19:01:25 -0700

    socket.io

--- routes/index.js
+++ routes/index.js
@@ -46,8 +46,6 @@ router.post('/search', function(req, res) {
 
 router.post('/article/save', function(req, res) {
 	// First, we grab the body of the html with request
-	console.log(req.body);
-
 	db.NYTSavedArticles.findOneAndUpdate(
 		{ headline: req.body.headline },
 		req.body,
@@ -58,19 +56,18 @@ router.post('/article/save', function(req, res) {
 	)
 		.then(function(NYTSavedArticle) {
 			// View the added result in the console
-			console.log(NYTSavedArticle);
+			req.app.socket.broadcast.emit('connection', true);
+			res.json(NYTSavedArticle);
 		})
 		.catch(function(err) {
 			// If an error occurred, send it to the client
-			return res.json(err);
+			res.json(err);
 		});
-	res.send('ok');
 });
 
 // Route for getting all Articles from the db
 router.get('/articles/saved', function(req, res) {
 	// Grab every document in the Articles collection
-	console.log('getting saved articles...')
 	db.NYTSavedArticles.find()
 		.then(function(dbNYTSavedArticles) {
 			// If we were able to successfully find Articles, send them back to the client
@@ -82,22 +79,20 @@ router.get('/articles/saved', function(req, res) {
 		});
 });
 
-// Route for saving/updating an Article's associated Comment
-router.put('/save/:id', function(req, res) {
-	// Create a new Comment and pass the req.body to the entry
-	db.NYTSavedArticles.findByIdAndUpdate(req.params.id, req.body, { new: true })
-		.then(function(dbNYTSavedArticles) {
-			// If we were able to successfully update an Article, send it back to the client
-			res.json(dbNYTSavedArticles);
-		})
-		.catch(function(err) {
-			// If an error occurred, send it to the client
-			res.json(err);
-		});
-});
+// router.put('/save/:id', function(req, res) {
+// 	db.NYTSavedArticles.findByIdAndUpdate(req.params.id, req.body, { new: true })
+// 		.then(function(dbNYTSavedArticles) {
+// 			// If we were able to successfully update an Article, send it back to the client
+// 			res.json(dbNYTSavedArticles);
+// 		})
+// 		.catch(function(err) {
+// 			// If an error occurred, send it to the client
+// 			res.json(err);
+// 		});
+// });
 
 // Route for grabbing a specific Article by id, populate it with it's Comment
-router.get('/latest/saved/comments/:id', function(req, res) {
+router.get('/article/saved/comments/:id', function(req, res) {
 	// Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
 	db.NYTSavedArticles.findOne({ _id: req.params.id })
 		// ..and populate all of the Comments associated with it
@@ -113,9 +108,10 @@ router.get('/latest/saved/comments/:id', function(req, res) {
 });
 
 // Route for saving/updating an Article's associated Comment
-router.post('/latest/saved/comment/:id', function(req, res) {
+router.post('/article/saved/comment/:id', function(req, res) {
+	console.log(req.body);
 	// Create a new Comment and pass the req.body to the entry
-	db.Comment.create(req.body)
+	db.Comments.create(req.body)
 		.then(function(dbComment) {
 			// If a Comment was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Comment
 			// { new: true } tells the query that we want it to return the updated User -- it returns the original by default
@@ -137,7 +133,7 @@ router.post('/latest/saved/comment/:id', function(req, res) {
 });
 
 router.delete('/saved/:id/comment/:comm_id', (req, res) => {
-	db.Comment.findByIdAndRemove(req.params.comm_id)
+	db.Comments.findByIdAndRemove(req.params.comm_id)
 		.then((dbComment) => {
 			return db.NYTSavedArticles.findByIdAndUpdate(
 				{ _id: req.params.id },
@@ -148,6 +144,19 @@ router.delete('/saved/:id/comment/:comm_id', (req, res) => {
 		.then((dbNYTSavedArticles) => {
 			const response = {
 				message: 'Comment successfully deleted',
+			};
+			return res.status(200).send(response);
+		})
+		.catch((err) => {
+			res.json(err);
+		});
+});
+
+router.delete('/article/saved/:id/', (req, res) => {
+	db.NYTSavedArticles.findByIdAndRemove(req.params.id)
+		.then(() => {
+			const response = {
+				message: 'Article successfully deleted',
 			};
 			return res.status(200).send(response);
 		})
