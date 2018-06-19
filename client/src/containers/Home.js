895914199b6431941490388d41d4d13336902742
madhavparekh <madhavparekh@gmail.com>
2018-06-18 19:03:20 -0700

    moved navbar to App.js

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 import Search from '../containers/Search';
 import Article from '../components/Article';
 
 class Home extends Component {
-	state = { articles: [] };
+	state = {
+		articles: [],
+		afterSearch: false,
+	};
 
 	onSubmitBtnHandler = (articles) => {
-		this.setState({ articles: articles });
+		this.setState({
+			articles: articles,
+			afterSearch: true,
+		});
 	};
 
 	render() {
 		return (
-			<div className="container">
-				<Navbar color="secondary" dark expand="md">
-					<NavbarBrand href="/">NY Times - Search Articles</NavbarBrand>
-					<Nav className="ml-auto" navbar>
-						<NavItem>
-							<NavLink href="/SavedArticles/">Saved Articles</NavLink>
-						</NavItem>
-					</Nav>
-				</Navbar>
-				<div className="containter mt-2">
-					<div className="row">
-						<div className="col col-md-3">
-							<Search onSubmitBtnHandler={(e) => this.onSubmitBtnHandler(e)} />
-						</div>
-						<div className="col col-md-9">
-							{this.state.articles ? (
-								this.state.articles.map((article, indx) => {
-									return <Article key={indx} article={article} />;
-								})
-							) : (
-								<div>No Articles found. Try differnt search</div>
-							)}
-						</div>
+			<div className="containter mt-2">
+				<div className="row">
+					<div className="col col-md-3">
+						<Search onSubmitBtnHandler={(e) => this.onSubmitBtnHandler(e)} />
+					</div>
+					<div className="col col-md-9">
+						{this.state.articles.length !== 0 ? (
+							this.state.articles.map((article, indx) => {
+								return <Article key={indx} article={article} />;
+							})
+						) : this.state.afterSearch ? (
+							<div className="container m-5">
+								<h2>No Articles found. Try differnt search</h2>
+							</div>
+						) : (
+							<div className="container m-5">
+								<h2>Welcome! Try searching... Dates are optional</h2>
+							</div>
+						)}
 					</div>
 				</div>
 			</div>
